e13a44e70b69e409d33567a15416b351cb5d6e93
berketurker <45368526+berkosko5000@users.noreply.github.com>
2023-02-09 16:52:55 +0300

    GenSwitch

--- Assets/Assets/Scripts/CharacterSwitch/CharacterSwitchController.cs
+++ Assets/Assets/Scripts/CharacterSwitch/CharacterSwitchController.cs
@@ -18,12 +18,14 @@ public class CharacterSwitchController : MonoBehaviour
         generationTwoDisk.SetActive(false);
         generationThreeDisk.SetActive(false);
         generationFourDisk.SetActive(false);
+        playerImage.sprite = genOneVisual;
         //Gen variable from gamemanager
         switch (gen)
         {
-            
+
             case 2:
                 generationTwoDisk.SetActive(true);
+                playerImage.sprite = genTwoVisual;
                 if (playerImage.sprite == genOneVisual)
                 {
                     genTwoButtonOne.interactable = false;
@@ -37,6 +39,7 @@ public class CharacterSwitchController : MonoBehaviour
                 break;
             case 3:
                 generationThreeDisk.SetActive(true);
+                playerImage.sprite = genThreeVisual;
                 if (playerImage.sprite == genOneVisual)
                 {
                     genThreeButtonOne.interactable = false;
@@ -55,9 +58,10 @@ public class CharacterSwitchController : MonoBehaviour
                     genThreeButtonTwo.interactable = true;
                     genThreeButtonThree.interactable = false;
                 }
-                break ;
+                break;
             case 4:
                 generationFourDisk.SetActive(true);
+                playerImage.sprite = genFourVisual;
                 if (playerImage.sprite == genOneVisual)
                 {
                     genFourButtonOne.interactable = false;
@@ -92,24 +96,96 @@ public class CharacterSwitchController : MonoBehaviour
     public void GenOneButton()
     {
         playerImage.sprite = genOneVisual;
-        genFourButtonFour.interactable = true;
+        switch (gen)
+        { 
+            case 2:
+                {
+                    genTwoButtonOne.interactable = false;
+                    genTwoButtonTwo.interactable = true;
+                }
+                break;
+
+            case 3:
+                {
+                    genThreeButtonOne.interactable = false;
+                    genThreeButtonTwo.interactable = true;
+                    genThreeButtonThree.interactable = true;
+                }
+                break;
+
+            case 4:
+                {
+                    genFourButtonOne.interactable = false;
+                    genFourButtonTwo.interactable = true;
+                    genFourButtonThree.interactable = true;
+                    genFourButtonFour.interactable = true;
+                }
+                break;
+        }
     }
 
     public void GenTwoButton()
     {
         playerImage.sprite = genTwoVisual;
-        genFourButtonFour.interactable = true;
+        switch (gen)
+        {
+
+            case 2:
+                {
+                    genTwoButtonOne.interactable = true;
+                    genTwoButtonTwo.interactable = false;
+                }
+                break;
+
+            case 3:
+                {
+                    genThreeButtonOne.interactable = true;
+                    genThreeButtonTwo.interactable = false;
+                    genThreeButtonThree.interactable = true;
+                }
+                break;
+
+            case 4:
+                {
+                    genFourButtonOne.interactable = true;
+                    genFourButtonTwo.interactable = false;
+                    genFourButtonThree.interactable = true;
+                    genFourButtonFour.interactable = true;
+                }
+                break;
+        }
     }
     public void GenThreeButton()
     {
         playerImage.sprite = genThreeVisual;
-        genFourButtonFour.interactable = true;
+        switch (gen)
+        {
+            case 3:
+                {
+                    genThreeButtonOne.interactable = true;
+                    genThreeButtonTwo.interactable = true;
+                    genThreeButtonThree.interactable = false;
+                }
+                break;
+
+            case 4:
+                {
+                    genFourButtonOne.interactable = true;
+                    genFourButtonTwo.interactable = true;
+                    genFourButtonThree.interactable = false;
+                    genFourButtonFour.interactable = true;
+                }
+                break;
+        }
     }
 
     public void GenFourButton()
     {
         playerImage.sprite = genFourVisual;
-
+        genFourButtonOne.interactable = true;
+        genFourButtonTwo.interactable = true;
+        genFourButtonThree.interactable = true;
+        genFourButtonFour.interactable = false;
     }
 
 }
